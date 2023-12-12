19b4db6c9f1d4cfd6a04f3bf351218fec9bb7049
NEstrada40 <125395715+NEstrada40@users.noreply.github.com>
2023-12-12 13:34:35 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,19 @@
-let player;
+let player; //Player Sprite Variable
+let playerCount = 0; //Amount of Player Sprite Copies -1
+let speedDrift = 0.05; //Variable for slowing to a stop after being in movement
+let maxSpeed = 3;
 
 function setup() {
-  new Canvas(windowWidth-60,windowHeight-60);
+  new Canvas(windowWidth,windowHeight);
+  camera.zoom = 1;
+
+  floor = new Sprite(windowWidth/2, windowHeight/2+50, 500, 40, 'static');
+  floor.color = 'blue';
+  world.gravity.y = 10;
 }
 
 function draw() {
   background(220);
-
     fill(0);
     spriteSpawn();
     spriteMove();
@@ -14,17 +21,50 @@ function draw() {
 
 
 function spriteSpawn() {
-  if (mouse.presses()) {
-    player = new Sprite(mouse.x, mouse.y, 30, 30)
-    player.vel.x = 0;
-    player.vel.y = 0;
-  }
+        if(playerCount === 0) {
+            player = new Sprite(windowWidth/2, windowHeight/2, 30, 30);
+            playerCount++;
+            player.vel.x = 0;
+            player.vel.y = 0;
+            player.rotationLock = true;
+            player.bounciness = 0;
+    }
 }
 
 function spriteMove() {
+  //slow to a stop after being in motion
+      if(player.vel.x > 0) {
+      player.vel.x -= speedDrift;
+      }
+      if(player.vel.x < 0) {
+      player.vel.x += speedDrift;
+      }
 
-    if(kb.pressing("w")) player.vel.y = -1;
-    if(kb.pressing("a")) player.vel.x = -1;
-    if(kb.pressing("s")) player.vel.y = 1;
-    if(kb.pressing("d")) player.vel.x = 1;
+      //keyboard input 'wasd'
+    if(player.colliding(floor)) {
+      if(kb.presses(" ")) { //up
+        player.vel.y = -6;
+        }
+      }
+
+    if(player.vel.x < maxSpeed && player.vel.x > -maxSpeed) { //so long as player movement is within max speed
+      if(kb.pressing("a")) { //left
+      player.vel.x -= 1;
+      }
+      if(kb.pressing("d")) { //right
+      player.vel.x += 1;
+      }
+    }
+
+    if(kb.pressing('shift')) {
+      maxSpeed = 6;
+    } else {
+      maxSpeed = 3;
+    }
+  }
+
+  function spriteAttack() {
+    if(player.triggersAttack()) {
+
+    }
   }
